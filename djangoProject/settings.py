a1e8493bca795f8b43ac3e9292e7e98d81083228
yejikim00 <kimyeji2902@gmail.com>
2022-03-29 12:27:59 +0900

    모델 생성

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pybo.apps.PyboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
