7f0fe03c29d465f503456d751e83a4a6107c437c
yejikim00 <kimyeji2902@gmail.com>
2022-03-29 17:07:01 +0900

    URL 별칭, 네임스페이스

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index),
-    path('<int:question_id>/', views.detail), # int: 숫자가 매핑됨을 의미
+    path('', views.index, name='index'), # index: http://localhost:8000/pybo/
+    path('<int:question_id>/', views.detail, name='detail'), # int: 숫자가 매핑됨을 의미, detail: http://localhost:8000/pybo/2
 ]
