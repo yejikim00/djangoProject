8e5188615afad960f88df81c741eee6bb8f37ae7
yejikim00 <kimyeji2902@gmail.com>
2022-04-09 16:22:25 +0900

    question, answer 등록에 폼 적용

--- pybo/urls.py
+++ pybo/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'), # index: http://localhost:8000/pybo/
     path('<int:question_id>/', views.detail, name='detail'), # int: 숫자가 매핑됨을 의미, detail: http://localhost:8000/pybo/2
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    path('question/create/', views.question_create, name='question_create'),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseNotAllowed
 from django.utils import timezone
 from .models import Question
+from .forms import QuestionForm, AnswerForm
 
 
 def index(request):
@@ -24,6 +26,33 @@ def detail(request, question_id):
 
 
 def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+
+def question_create(request):
+    if request.method == 'POST':    # 데이터 입력이 끝나 '저장하기=submit'을 누르면 post요청으로 바뀜.
+        form = QuestionForm(request.POST)   # f답rm에 데이터 저장, request.POST: 사용자가 입력한 내용이 담김.
+        if form.is_valid():
+            question = form.save(commit=False)  # commit=False: 임시 저장
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')   # 데이터 저장 후 다시 질문 목록 보여주기 위해 index 불러옴.
+    else:   # 처음엔 url밖에 전해주지 않으니 무조건 GET요청. 데이터 담을 수 있는 form을 불러옴.
+        form = QuestionForm()
+    context = {'form': form}    # {'form': form}: 템플릿에서 질문 등록시 사용할 폼 엘리먼트 생성.
+    return render(request, 'pybo/question_form.html', context)  # 이게 데이터 입력하는 form창 불러오는 것.
