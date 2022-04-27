6f6028d691ae63985e9d7188c4caae7a7d43d466
yejikim00 <kimyeji2902@gmail.com>
2022-04-27 16:18:51 +0900

    modified settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -22,22 +22,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os, json
 from django.core.exceptions import ImproperlyConfigured
 
-# secret_file = os.path.join(BASE_DIR, 'secrets.json')
-#
-# with open(secret_file) as f:
-#     secrets = json.loads(f.read())
-#
-#
-# def get_secret(setting, secrets=secrets):
-#     try:
-#         return secrets[setting]
-#     except KeyError:
-#         error_msg = "Set the {} environment variable".format(setting)
-#         raise ImproperlyConfigured(error_msg)
-#
-#
-# SECRET_KEY = get_secret("SECRET_KEY")
-SECRET_KEY = 'h=smhlk$nulmew5kvv)ko3l7sqsir(^71n$)788ct(&8f)na^-'
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
