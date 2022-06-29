266f5fd2f34fc4494c999af98e99ebcd6d68645b
yejikim00 <kimyeji2902@gmail.com>
2022-06-29 17:21:20 +0900

    로그인, 로그아웃 기능 구현

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -137,3 +137,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 로그인 성공 후 이동하는 URL
+LOGIN_REDIRECT_URL = '/'
+
+# 로그아웃시 이동하는 URL
+LOGOUT_REDIRECT_URL = '/'
