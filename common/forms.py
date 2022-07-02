fcba8cc6f1ac851519064b4a980cc5cbc4a258c1
yejikim00 <kimyeji2902@gmail.com>
2022-07-02 23:17:37 +0900

    파이보 회원가입 기능 구현

--- common/forms.py
+++ common/forms.py
@@ -7,5 +7,5 @@ class UserForm(UserCreationForm):
     email = forms.EmailField(label="이메일")
 
     class Meta:
-        model = User
-        fields = ("username", "password1", "password2", "email")
+        model = User  # 사용할 모델
+        fields = ("username", "password1", "password2", "email")   # UserForm에서 사용할 UserCreationForm 모델의 속성.
