8bbb7c63868e1c4dd3e55b5b6e0b776d8d8e46cd
yejikim00 <kimyeji2902@gmail.com>
2022-07-06 21:07:39 +0900

    views.py 파일 분리 두 번째 방법

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from pybo import views
+from pybo.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
-    path('', views.index, name='index'),  # '/'에 해당되는 path = 'http://127.0.0.1:8000'
+    path('', base_views.index, name='index'),  # '/'에 해당되는 path = 'http://127.0.0.1:8000'
 ]
